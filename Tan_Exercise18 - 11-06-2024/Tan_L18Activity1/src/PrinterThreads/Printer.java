79458e6ae91162d0789e3e4d3ca3b3de2dc4026a
kyle-tan <kyle.tan@awsys-i.com>
2024-11-06 18:01:10 +0800

    Added method contents

--- Tan_Exercise18 - 11-06-2024/Tan_L18Activity1/src/PrinterThreads/Printer.java
+++ Tan_Exercise18 - 11-06-2024/Tan_L18Activity1/src/PrinterThreads/Printer.java
@@ -8,9 +8,19 @@ public class Printer implements Runnable{
 	}
 	
 	private void printDocuments() {
-		
+		System.out.println("Printing: " + getDocumentName());
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			
+		}
+		System.out.println("Finished printing: " + getDocumentName());
 	}
 	
+	public String getDocumentName() {
+		return documentName;
+	}
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
