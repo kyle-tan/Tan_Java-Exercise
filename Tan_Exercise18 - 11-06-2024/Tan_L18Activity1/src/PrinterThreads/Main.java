419968e21f48e5ed76e39a361c316647976cb019
kyle-tan <kyle.tan@awsys-i.com>
2024-11-06 17:47:53 +0800

    Added partial Printer Thread

---
+++ Tan_Exercise18 - 11-06-2024/Tan_L18Activity1/src/PrinterThreads/Main.java
@@ -0,0 +1,8 @@
+package PrinterThreads;
+
+public class Main {
+	public static void main(String[] args) {
+		Thread thread1 = new Thread();
+		Thread thread2 = new Thread();
+	}
+}
--- Tan_Exercise18 - 11-06-2024/Tan_L18Activity1/src/PrinterThreads/Printer.java
+++ Tan_Exercise18 - 11-06-2024/Tan_L18Activity1/src/PrinterThreads/Printer.java
@@ -1,5 +1,20 @@
 package PrinterThreads;
 
-public class Printer {
+public class Printer implements Runnable{
+	private String documentName;
+	
+	public Printer(String documentName) {
+		this.documentName = documentName;
+	}
+	
+	private void printDocuments() {
+		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		printDocuments();
+	}
 
 }
